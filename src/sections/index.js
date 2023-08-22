0759e400e5a1719c3c5238995fabd47cbbea9048
Ayesha <ayeshasolanki151@gmail.com>
2023-08-22 23:15:14 +0530

    Source code added

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+
+export {
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+  Hero,
+  CustomerReviews,
+  Footer,
+  PopularProducts
+}
